eb40a056793d6c8416e3227f3bd62c4ccf0d74a1
TheSomeshKumar <thesomeshkumar@gmail.com>
2023-01-20 19:35:21 +0530

    added bottom navigation bar & movie tab

--- app/src/main/java/com/thesomeshkumar/tmdb/ui/MainActivity.kt
+++ app/src/main/java/com/thesomeshkumar/tmdb/ui/MainActivity.kt
@@ -2,6 +2,12 @@ package com.thesomeshkumar.tmdb.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.thesomeshkumar.tmdb.R
 import com.thesomeshkumar.tmdb.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,5 +20,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        val navView: BottomNavigationView = binding.navView
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.navigation_tv_show, R.id.navigation_movies)
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
